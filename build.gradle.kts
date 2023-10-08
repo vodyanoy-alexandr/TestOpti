14a6b7e4ce34a2c2d2032ff5fb2f649b3bc267be
Alexandr Vodyanoy <vodyanoy.alexandr@gmail.com>
2023-10-08 22:49:45 +0300

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
     testImplementation("com.codeborne:selenide:6.19.0")
     testImplementation("io.rest-assured:rest-assured:5.3.2")
+    testImplementation("org.apache.logging.log4j:log4j-core:2.14.1")
+    //testImplementation("org.slf4j:slf4j-simple:2.0.9") // логи при выполнении тестов ui
 }
 
 tasks.test {
