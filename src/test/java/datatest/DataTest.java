836bc46e5e87f1a7095e237fce6ba9f2a996c51a
Alexandr Vodyanoy <vodyanoy.alexandr@gmail.com>
2023-10-12 17:39:11 +0300

    update

--- src/test/java/datatest/DataTest.java
+++ src/test/java/datatest/DataTest.java
@@ -8,9 +8,9 @@ import pages.AuthPage;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.clearBrowserCache;
 
-// класс с базовыми тестами
+// класс с базовыми данными
 public class DataTest {
-    private static String urlStand = "https://master.dc.oswfm.ru"; // базовый url
+    private static String urlStand = "https://.dc.oswfm.ru"; // базовый url
 
     public String getUrlStand() {
         return urlStand;
