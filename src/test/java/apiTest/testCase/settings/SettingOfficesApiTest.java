28bfe6109e18bddb137cb45cd45529f0a50d130e
Alexandr Vodyanoy <vodyanoy.alexandr@gmail.com>
2023-11-16 14:08:56 +0300

    обновил тесты, дописал тудушки

--- src/test/java/apiTest/testCase/settings/SettingOfficesApiTest.java
+++ src/test/java/apiTest/testCase/settings/SettingOfficesApiTest.java
@@ -3,6 +3,7 @@ package apiTest.testCase.settings;
 import apiTest.model.settings.office.SettingOfficesBodyModel;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.javafaker.Faker;
 import dataTest.DataTest;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
@@ -10,15 +11,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Locale;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.notNullValue;
 
 public class SettingOfficesApiTest {
     DataTest dataTest = new DataTest();
+    static Faker faker = new Faker(new Locale("ru"));
+    private final static String nameOffice = faker.address().cityName();
 
     @BeforeEach
     public void setup() {
         RestAssured.baseURI = dataTest.getUrlStand();
+        // todo добавить запрос на авторизацию и результат ложить в token, cookie
     }
 
     @DisplayName("Тест на чтение всех офисов в системе через api ")
@@ -52,6 +58,7 @@ public class SettingOfficesApiTest {
 
         // Десериализация JSON в объект bodyModel
         SettingOfficesBodyModel bodyModel = objectMapper.readValue(json, SettingOfficesBodyModel.class);
+        bodyModel.setName(nameOffice);
 
         given()
                 .log().all()
@@ -72,9 +79,9 @@ public class SettingOfficesApiTest {
     @Test
     public void deleteOfficeTest() {
 
-        String requestBody = "{\"id\":\"9a66c660-0df5-4b6d-b64e-c0f85b6e95a8\"}"; // id удаляемого офиса todo вынести в модель
-        String token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJSQkVCUlRkb0NWcW5KZlZTTkh1ckQ4bGs1MVIzUGw0aXJwbE1BU0JXM0l3In0.eyJleHAiOjE3MDAxMjQ5NDMsImlhdCI6MTcwMDEyNDY0MywiYXV0aF90aW1lIjoxNzAwMTI0NjQwLCJqdGkiOiI2ZGQ0MGE2Mi00MjRlLTQ5NTEtYTVlMi01MzI3OTg2MTJjN2MiLCJpc3MiOiJodHRwczovL2tjLmRjLm9zd2ZtLnJ1L3JlYWxtcy9vc19tYXN0ZXIiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiYTU4NmRkNTgtZDMwMy00OTRmLWIwNGMtNDM3ODQ1M2I2MmUwIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoid2ZtLWZyb250ZW5kIiwibm9uY2UiOiJlNjM0M2I0Zi02ZjJmLTQzZTktODkzMi1jYmU5ZTkyNzIwOTkiLCJzZXNzaW9uX3N0YXRlIjoiMTY3NTFjMzEtNWY3NC00ZDVkLTg2YTQtOWUwM2E2MjdjM2Y0IiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyIqIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJkZWZhdWx0LXJvbGVzLW9zbiIsIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwic2lkIjoiMTY3NTFjMzEtNWY3NC00ZDVkLTg2YTQtOWUwM2E2MjdjM2Y0IiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJhZG1pbiIsImxvY2FsZSI6InJ1In0.K2lPBVHH0Mv8VaPeboM6_5ufAngv1Wjv9-WxL1IEXR5s_NQK0lE23_3Q5zNiACVWvFde1QRp9rRuDic_yePIAWrDQJ5oDfwyoqCp6VQ6ymCQqWzHYFv47wFtidmK53IwPYgfixPqB-x-jnDwIaY1uk2VTWscVFMxjVIUqCmHhtgBl3of2jsE5mdgmeLCA-e1DUiP4Jsi8CbCPTHu9yA6b53KGqxPzczFL4ImXrD2uI6Pihd7iweR0Dx4hlqfG_5blFDZdq3_5r5J2z3UV4SXRcwwL45weWk22aLu28V5QBJB1xhGIDkLhQ9DSQzXR71JbshwP-_wjrlJBlwQvHAfiA"; // todo  token
-        String cookie = "INGRESSCOOKIE=1697524826.506.12342.103057|27d02e9bd0328ccdb383216fe38836d0; INGRESSCOOKIE=1697524838.057.12342.570613|8d78fad643bac180d9fd7b95667074dc; rl_anonymous_id=RudderEncrypt%3AU2FsdGVkX19Y1CCDT0cgSy6jtkTxWWjNToRL0fwVep%2FWiq4GiiKq64ZwDgIASVLK2%2FcOSWccWzRUIg4Li1Syig%3D%3D; rl_group_id=RudderEncrypt%3AU2FsdGVkX1%2FG0C4Olqf%2B0xx0q%2BrUMVT7FxdfeLzMHsY%3D; rl_group_trait=RudderEncrypt%3AU2FsdGVkX19Ln3AqLtkhFOtQZUTDqU7Pop8lrLlqxlg%3D; rl_user_id=RudderEncrypt%3AU2FsdGVkX19FvqvkxJhX0%2FrCCR1hFgtTr4moGDnQRFiikizr9jBWTZFSAC0%2BQIdj; rl_trait=RudderEncrypt%3AU2FsdGVkX19fw%2BaPneP76dDo8douX%2BYKXKDo95oxrss%3D; @wfmfront-token=eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJSQkVCUlRkb0NWcW5KZlZTTkh1ckQ4bGs1MVIzUGw0aXJwbE1BU0JXM0l3In0.eyJleHAiOjE3MDAxMjUwNzIsImlhdCI6MTcwMDEyNDc3MiwiYXV0aF90aW1lIjoxNzAwMTI0NjQwLCJqdGkiOiIxM2RkZGJmZC03YjUyLTQ0NzEtYjk2OC0wMDE0MGVlOWM3M2EiLCJpc3MiOiJodHRwczovL2tjLmRjLm9zd2ZtLnJ1L3JlYWxtcy9vc19tYXN0ZXIiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiYTU4NmRkNTgtZDMwMy00OTRmLWIwNGMtNDM3ODQ1M2I2MmUwIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoid2ZtLWZyb250ZW5kIiwibm9uY2UiOiJlNjM0M2I0Zi02ZjJmLTQzZTktODkzMi1jYmU5ZTkyNzIwOTkiLCJzZXNzaW9uX3N0YXRlIjoiMTY3NTFjMzEtNWY3NC00ZDVkLTg2YTQtOWUwM2E2MjdjM2Y0IiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyIqIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJkZWZhdWx0LXJvbGVzLW9zbiIsIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwic2lkIjoiMTY3NTFjMzEtNWY3NC00ZDVkLTg2YTQtOWUwM2E2MjdjM2Y0IiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJhZG1pbiIsImxvY2FsZSI6InJ1In0.9bnXMp_7csEt0-lEdMwRSCcn_WE9Jn1T5Ypc8J6pWlHZQQv02wMIDzuqL2UIMg3i3XQ58FYJ91ao4owhRm5x8g61snin-0f86hdvX7scUKd78Y_py0BKFbZLcw98aWDerSbtANqSH7a0TNP-1INYi5TYkJAP1l9XgeeeKSU0d7zxUsuILbbMdLGS6eBV0bTfSE_CnssioosyjZUhBLEiLbeDArlu75jNHQmKEeiiFrLOAbovGQ3xBv-qmfbsFuU_QCbCQ28W13SJYOjskovRk7XOltjx10a3EMI1th-VZ2j0EXSUpiAWT8rDTepe6xfDPhhYByo10DGiaVsWpTjGqg"; // todo сюда надо добавить cookie
+        String requestBody = "{\"id\":\"9a66c660-0df5-4b6d-b64e-c0f85b6e95a8\"}"; // id удаляемого офиса todo вынести в модель или получать id офиса из вне или запроса на создание офиса
+        String token = ""; // todo  token
+        String cookie = ""; // todo сюда надо добавить cookie
 
         Response response =
                 given()
@@ -90,8 +97,6 @@ public class SettingOfficesApiTest {
                         .statusCode(200) // Проверка кода состояния HTTP
                         .extract()
                         .response();
-        // Дополнительные проверки
-        // Например, проверка содержимого ответа или других параметров
         // response.then().requestBody("some_key", equalTo("expected_value"));
     }
 }
