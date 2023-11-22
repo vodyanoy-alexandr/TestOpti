65b3e638d5d77aa2d80fb8dbe956c0e6a533028a
Alexandr Vodyanoy <vodyanoy.alexandr@gmail.com>
2023-11-22 10:19:30 +0300

    вынес данные для авторизации в конфиг

--- src/test/java/dataTest/BaseDataTest.java
+++ src/test/java/dataTest/BaseDataTest.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+// класс с базовыми данными
 public class BaseDataTest {
 
     private static final Properties properties = new Properties();
