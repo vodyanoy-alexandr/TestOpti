7b121df27388ce439a80b401bb13255aaebb2a1e
Alexandr Vodyanoy <vodyanoy.alexandr@gmail.com>
2023-11-22 10:10:24 +0300

    update

--- src/test/java/apiTest/model/settings/office/SettingOfficesBodyModel.java
+++ src/test/java/apiTest/model/settings/office/SettingOfficesBodyModel.java
@@ -1,20 +1,63 @@
 package apiTest.model.settings.office;
 
 public class SettingOfficesBodyModel {
-    //
-    private WorkTimeOfficesBodyModel[] workTime;
+    //    {
+//    "workTime": [
+//        {
+//            "dayOfWeek": 1,
+//            "from": "08:00",
+//            "to": "20:00",
+//            "weekend": false
+//        },
+//        {
+//            "dayOfWeek": 2,
+//            "from": "08:00",
+//            "to": "20:00",
+//            "weekend": false
+//        },
+//        {
+//            "dayOfWeek": 3,
+//            "from": "08:00",
+//            "to": "20:00",
+//            "weekend": false
+//        },
+//        {
+//            "dayOfWeek": 4,
+//            "from": "08:00",
+//            "to": "20:00",
+//            "weekend": false
+//        },
+//        {
+//            "dayOfWeek": 5,
+//            "from": "08:00",
+//            "to": "20:00",
+//            "weekend": false
+//        },
+//        {
+//            "dayOfWeek": 6,
+//            "from": "08:00",
+//            "to": "20:00",
+//            "weekend": true
+//        },
+//        {
+//            "dayOfWeek": 7,
+//            "from": "08:00",
+//            "to": "20:00",
+//            "weekend": true
+//        }
+//    ],
+//    "name": "new office",
+//    "timezone": "Europe/Moscow",
+//    "comment": "comments",
+//    "maxSeats": 777
+//}
     private String name;
     private String timezone;
     private String comment;
     private int maxSeats;
+    private WorkTime[] workTime;
 
-    public WorkTimeOfficesBodyModel[] getWorkTime() {
-        return workTime;
-    }
-
-    public void setWorkTime(WorkTimeOfficesBodyModel[] workTime) {
-        this.workTime = workTime;
-    }
+    // Геттеры и сеттеры для полей
 
     public String getName() {
         return name;
@@ -47,5 +90,54 @@ public class SettingOfficesBodyModel {
     public void setMaxSeats(int maxSeats) {
         this.maxSeats = maxSeats;
     }
-}
 
+    public WorkTime[] getWorkTime() {
+        return workTime;
+    }
+
+    public void setWorkTime(WorkTime[] workTime) {
+        this.workTime = workTime;
+    }
+
+    // Вложенный класс WorkTime для представления данных о рабочем времени
+    public static class WorkTime {
+        private int dayOfWeek;
+        private String from;
+        private String to;
+        private boolean weekend;
+
+        // Геттеры и сеттеры для полей
+
+        public int getDayOfWeek() {
+            return dayOfWeek;
+        }
+
+        public void setDayOfWeek(int dayOfWeek) {
+            this.dayOfWeek = dayOfWeek;
+        }
+
+        public String getFrom() {
+            return from;
+        }
+
+        public void setFrom(String from) {
+            this.from = from;
+        }
+
+        public String getTo() {
+            return to;
+        }
+
+        public void setTo(String to) {
+            this.to = to;
+        }
+
+        public boolean isWeekend() {
+            return weekend;
+        }
+
+        public void setWeekend(boolean weekend) {
+            this.weekend = weekend;
+        }
+    }
+}
--- src/test/java/dataTest/BaseDataTest.java
+++ src/test/java/dataTest/BaseDataTest.java
@@ -1,13 +1,28 @@
 package dataTest;
 
-// класс с базовыми данными
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class BaseDataTest {
 
-    private static final String LOGIN_MANAGER = "manager";
-    private static final String PASS_MANAGER = "manager";
-    private static final String LOGIN_OPERATOR = "operator";
-    private static final String PASS_OPERATOR = "operator";
-    private static final String URL_STAND = "https://master.dc.oswfm.ru";
+    private static final Properties properties = new Properties();
+
+    static {
+        try {
+            properties.load(new FileInputStream("config.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static final String LOGIN_MANAGER = properties.getProperty("LOGIN_MANAGER");
+    private static final String PASS_MANAGER = properties.getProperty("PASS_MANAGER");
+    private static final String LOGIN_OPERATOR = properties.getProperty("LOGIN_OPERATOR");
+    private static final String PASS_OPERATOR = properties.getProperty("PASS_OPERATOR");
+    private static final String URL_STAND = properties.getProperty("URL_STAND");
+
     public String getLoginManager() {
         return LOGIN_MANAGER;
     }
@@ -28,5 +43,6 @@ public class BaseDataTest {
         return URL_STAND;
     }
 }
+
 
 
